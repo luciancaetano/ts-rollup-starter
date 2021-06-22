04033d052f18d7daeca0b8db1c7e3e5c96802a30
Lucian Caetano <lucian.santos.caetano@gmail.com>
2021-06-21 22:58:53 -0300

    Reset sources

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import { readYmalFile, s } from './utils';
-
-s(async () => {
-    const dnsConf = await readYmalFile(path.join(process.cwd(), 'buildconf.yaml'));
-
-    console.log(JSON.stringify(dnsConf, null, '\t'));
-});
--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import yaml from 'yaml';
-import { promises as fs } from 'fs';
-
-export const readYmalFile = async (fileName: string, encoding: BufferEncoding = 'utf-8') => yaml.parse((await (await fs.readFile(fileName)).toString(encoding)));
-
-export const s = async (fn: () => Promise<void>) => {
-    await fn();
-};
